153a201fbe0f2988f6fd81addef556da4defd2ae
hltdal <dalha21@itu.edu.tr>
2025-11-21 02:28:26 +0300

    robot's model is seen both rviz and gazebo

--- src/set_slam/setup.py
+++ src/set_slam/setup.py
@@ -14,7 +14,7 @@ setup(
         (os.path.join('share', package_name, 'launch'), glob('set_slam/launch/*.py')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
-        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*.stl')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
